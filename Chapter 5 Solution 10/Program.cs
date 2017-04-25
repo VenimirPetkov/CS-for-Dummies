164c1576e34fb301466d4f76851ce6ec8306929f
VenimirPetkov <venimirp@gmail.com>
2017-04-26 02:09:20 +0300

    Use Switch
    Condition is to use switch

--- Chapter 5 Solution 10/Program.cs
+++ Chapter 5 Solution 10/Program.cs
@@ -1,23 +1,39 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Chapter_5_Solution_10
+namespace ConsoleApp9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            byte points;
-            Console.Write("Enter points between 1 and 9: ");
-            points = Convert.ToByte(Console.ReadLine());
-            if (points >= 1 && points <= 3)
-                Console.WriteLine("Points multiplied by 10: {0}", points * 10);
-            else if (points >= 4 && points <= 6)
-                Console.WriteLine("Points multiplied by 100: {0}", points * 100);
-            else if (points >= 7 && points <= 9)
-                Console.WriteLine("Points multiplied by 1000: {0}", points * 1000);
-            else
-                Console.WriteLine("Wrong Input!");
-            Console.ReadLine();
+            Console.WriteLine("Write a Number from 1 to 9");
+            byte points = byte.Parse(Console.ReadLine());
+            switch (points)
+            {
+                case 1: 
+                case 2: 
+                case 3:
+                    Console.WriteLine("{0}", points * 10);
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("{0}", points * 100);
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                    Console.WriteLine("{0}", points * 1000);
+                    break;
+                default:
+                    Console.WriteLine("Error imput a number from 1 to 9");
+                    break;
+
+            }
         }
     }
 }
